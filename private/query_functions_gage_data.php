7195d8e8021d6b9f47834c50ecee028112751cc1
ivan nguyen <ivan.h.nguyen@usace.army.mil>
2024-07-01 14:12:37 -0500

    added function find_nws_forecast2

--- private/query_functions_gage_data.php
+++ private/query_functions_gage_data.php
@@ -306,4 +306,143 @@ function get_table_data($db, $cwms_ts_id, $start_day, $end_day) {
 	}
 	return $data;
 }
+//------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------
+function find_nws_forecast2($db, $cwms_ts_id, $nws_day1_date, $nws_day2_date, $nws_day3_date) {
+	$stmnt_query = null;
+	$data = null;
+	try {		
+		$sql = "with cte_day1 as (select cwms_util.change_timezone(date_time, 'UTC', 'CST6CDT') as date_time
+					,value
+					,cwms_ts_id
+					,cwms_util.split_text(cwms_ts_id, 1, '.') as location_id
+					,unit_id
+					,to_char(cwms_util.change_timezone(data_entry_date, 'UTC', 'CST6CDT'), 'mm/dd HH24:MI') as data_entry_date
+					,to_char(cwms_util.change_timezone(data_entry_date, 'UTC', 'CST6CDT'), 'mm-dd-yyyy HH24:MI') as data_entry_date_org
+				from CWMS_20.AV_TSV_DQU
+				where cwms_ts_id = '".$cwms_ts_id."'
+					and unit_id = 'ft'
+					and date_time = to_date('".$nws_day1_date."' || '12:00' ,'mm-dd-yyyy hh24:mi')
+				),
+				
+				day_2 as (
+				select cwms_util.change_timezone(date_time, 'UTC', 'CST6CDT') as date_time
+					,value
+					,cwms_ts_id
+					,cwms_util.split_text(cwms_ts_id, 1, '.') as location_id
+					,unit_id
+					,to_char(cwms_util.change_timezone(data_entry_date, 'UTC', 'CST6CDT'), 'mm/dd HH24:MI') as data_entry_date
+					,to_char(cwms_util.change_timezone(data_entry_date, 'UTC', 'CST6CDT'), 'mm-dd-yyyy HH24:MI') as data_entry_date_org
+				from CWMS_20.AV_TSV_DQU
+				where cwms_ts_id = '".$cwms_ts_id."'
+					and unit_id = 'ft'
+					and date_time = to_date('".$nws_day2_date."' || '12:00' ,'mm-dd-yyyy hh24:mi')
+				),
+				
+				day_3 as (
+				select cwms_util.change_timezone(date_time, 'UTC', 'CST6CDT') as date_time
+					,value
+					,cwms_ts_id
+					,cwms_util.split_text(cwms_ts_id, 1, '.') as location_id
+					,unit_id
+					,to_char(cwms_util.change_timezone(data_entry_date, 'UTC', 'CST6CDT'), 'mm/dd HH24:MI') as data_entry_date
+					,to_char(cwms_util.change_timezone(data_entry_date, 'UTC', 'CST6CDT'), 'mm-dd-yyyy HH24:MI') as data_entry_date_org
+				from CWMS_20.AV_TSV_DQU
+				where cwms_ts_id = '".$cwms_ts_id."'
+					and unit_id = 'ft'
+					and date_time = to_date('".$nws_day3_date."' || '12:00' ,'mm-dd-yyyy hh24:mi')
+				)
+				
+				select day1.date_time as date_time_day1, day1.value as value_day1, day1.cwms_ts_id as cwms_ts_id_day1, day1.location_id as location_id_day1, day1.unit_id as unit_id_day1, day1.data_entry_date as data_entry_date_day1, day1.data_entry_date_org as data_entry_date_org_day1
+					,day2.date_time as date_time_day2, day2.value as value_day2, day2.cwms_ts_id as cwms_ts_id_day2, day2.location_id as location_id_day2, day2.unit_id as unit_id_day2, day2.data_entry_date as data_entry_date_day2, day2.data_entry_date_org as data_entry_date_org_day2
+					,day3.date_time as date_time_day3, day3.value as value_day3, day3.cwms_ts_id as cwms_ts_id_day3, day3.location_id as location_id_day3, day3.unit_id as unit_id_day3, day3.data_entry_date as data_entry_date_day3, day3.data_entry_date_org as data_entry_date_org_day3
+				from cte_day1 day1
+					left join day_2 day2
+					on day1.location_id = day2.location_id
+						left join day_3 day3
+						on day1.location_id = day3.location_id
+				";
+		
+		$stmnt_query = oci_parse($db, $sql);
+		$status = oci_execute($stmnt_query);
+
+		while (($row = oci_fetch_array($stmnt_query, OCI_ASSOC+OCI_RETURN_NULLS)) !== false) {
+			
+			$data = (object) [
+				"date_time_day1" => $row['DATE_TIME_DAY1'],
+				"data_entry_date_day1" => $row['DATA_ENTRY_DATE_DAY1'],
+				"data_entry_date_org_day1" => $row['DATA_ENTRY_DATE_ORG_DAY1'],
+				"value_day1" => $row['VALUE_DAY1'],
+				"unit_id_day1" => $row['UNIT_ID_DAY1'],
+				"cwms_ts_id_day1" => $row['CWMS_TS_ID_DAY1'],
+				"location_id_day1" => $row['LOCATION_ID_DAY1'],
+				"date_time_day2" => $row['DATE_TIME_DAY2'],
+				"data_entry_date_day2" => $row['DATA_ENTRY_DATE_DAY2'],
+				"data_entry_date_org_day2" => $row['DATA_ENTRY_DATE_ORG_DAY2'],
+				"value_day2" => $row['VALUE_DAY2'],
+				"unit_id_day2" => $row['UNIT_ID_DAY2'],
+				"cwms_ts_id_day2" => $row['CWMS_TS_ID_DAY2'],
+				"location_id_day2" => $row['LOCATION_ID_DAY2'],
+				"date_time_day3" => $row['DATE_TIME_DAY3'],
+				"data_entry_date_day3" => $row['DATA_ENTRY_DATE_DAY3'],
+				"data_entry_date_org_day3" => $row['DATA_ENTRY_DATE_ORG_DAY3'],
+				"value_day3" => $row['VALUE_DAY3'],
+				"unit_id_day3" => $row['UNIT_ID_DAY3'],
+				"cwms_ts_id_day3" => $row['CWMS_TS_ID_DAY3'],
+				"location_id_day3" => $row['LOCATION_ID_DAY3']
+			];
+			
+		}
+	}
+	catch (Exception $e) {
+		$e = oci_error($db);  
+		trigger_error(htmlentities($e['message']), E_USER_ERROR);
+
+		return null;
+	}
+	finally {
+		oci_free_statement($stmnt_query); 
+	}
+	return $data;
+}
+//------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------
+function get_record_stage2($db, $cwms_ts_id) {
+	$stmnt_query = null;
+	$data = null;
+	
+	try {		
+		$sql = "select location_id
+					,location_level_id
+					,level_date
+					,constant_level
+					,level_unit
+				from CWMS_20.AV_LOCATION_LEVEL
+				where specified_level_id = 'Record Stage' and location_id = cwms_util.split_text('".$cwms_ts_id."', 1, '.') and unit_system = 'EN'";
+		
+		$stmnt_query = oci_parse($db, $sql);
+		$status = oci_execute($stmnt_query);
+
+		while (($row = oci_fetch_array($stmnt_query, OCI_ASSOC+OCI_RETURN_NULLS)) !== false) {
+			$data = (object) [
+				"location_id" => $row['LOCATION_ID'],
+				"location_level_id" => $row['LOCATION_LEVEL_ID'],
+				"level_date" => $row['LEVEL_DATE'],
+				"constant_level" => $row['CONSTANT_LEVEL'],
+				"level_unit" => $row['LEVEL_UNIT']
+			];
+		}
+	}
+	catch (Exception $e) {
+		$e = oci_error($db);  
+		trigger_error(htmlentities($e['message']), E_USER_ERROR);
+		return null;
+	}
+	finally {
+		oci_free_statement($stmnt_query); 
+	}
+	return $data;
+}
 ?>
